f09fccef91bea7f187cee8115c6192f595ec07ae
Fredrik Nystrand <f.nystrand@gmail.com>
2022-05-05 12:04:12 +0200

    navbar

--- Frontend/ecom/src/App.jsx
+++ Frontend/ecom/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShopView from './views/ShopView';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <ShopView />
+      <BrowserRouter>
+        <Navbar />
+        <ShopView />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- Frontend/ecom/src/components/Navbar.jsx
+++ Frontend/ecom/src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar__links">
+          <NavLink to="/" className="nav-link">Shop</NavLink>
+          <NavLink to="/admin" className="nav-link">Admin</NavLink>
+        </div>
+        <div className="navbar__logo">
+          LOGOTYP
+        </div>
+        <div className="navbar__actions">
+          <button className="btn navbar-btn"><i class="fa-solid fa-user"></i>Login</button>
+          <div className="cart-btn">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
