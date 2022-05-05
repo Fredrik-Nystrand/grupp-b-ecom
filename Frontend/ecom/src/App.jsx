b3a3097a6fc27ef060d635bc9ee133714567f04d
Fredrik Nystrand <f.nystrand@gmail.com>
2022-05-05 10:40:03 +0200

    start

--- Frontend/ecom/src/App.jsx
+++ Frontend/ecom/src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css';
-import jwt_decode from "jwt-decode";
+import ShopView from './views/ShopView';
+import Navbar from './components/Navbar';
 
-const decodeToken = (token) => {      
-  const decode = jwt_decode(token);
-  if (decode.exp * 1000 < new Date().getTime()) {
-      console.log('Expired');
-  }
-};
-
-decodeToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNzIyNzJjYmM2M2RiMjVlY2RhOGUxYiIsImlhdCI6MTY1MTY1OTAzNCwiZXhwIjoxNjUxNjU5MDM1fQ.L9Luxg0WRcYlmexL3800Skz7wnP7SZ5n7l3rii-OWt0');
 
 function App() {
   return (
     <div className="App">
-      ... börja design här ...
+      <Navbar />
+      <ShopView />
     </div>
   );
 }
