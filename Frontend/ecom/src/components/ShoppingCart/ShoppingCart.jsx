93340f13461e8b947ca262dfa782278aef07fbaf
Fredrik Nystrand <f.nystrand@gmail.com>
2022-05-17 15:51:10 +0200

    auth

--- Frontend/ecom/src/components/ShoppingCart/ShoppingCart.jsx
+++ Frontend/ecom/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import CartItem from './CartItem'
 
